ac1a21070b62cd65e97adff975d3b1da5ca6903f
Mandar1jn <Marijnkneppers@gmail.com>
2022-12-03 18:36:07 +0100

    Revert "Setup for deployment"
    This reverts commit 5d077102bc9ce37c06f3bb70e0bca3cf396550b6.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	"site": "http://mandar1jn.github.io",
-	"base": "portal-emu-interface"
-});
+export default defineConfig({});
